4448771d33133d523b3f1e9dfdd5a42c35cea746
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-11-22 12:20:19 +0500

    gsgsh

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 import json
-
+a=[]
+s=0
 response = requests.get('https://randomuser.me/api/')
 
 if response.status_code == 200:
     content = response.content
     data = json.loads(content.decode())
-
-    print(data)
-    print(type(data))
+# user={"fulname":data["results"][0]['first']+' '+data["results"][0]['first']}
+randomuser=data["results"][0]
+for i in randomuser:
+    a.append({"fulname":i["name"]["first"]+i["name"]["last"],
+               "email":i["email"],
+               "phone":i["phone"]})
+    s+=1
+b=open("users.json","w")
+b.write(json.dumps(a,indent=4))
